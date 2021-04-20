c3eef880eb2d1b52eaf48937520edbc143b0c0f7
AndreaGaggia <gaggiandrea@gmail.com>
2021-04-20 10:52:26 +0200

    render library view

--- src/views/Libreria.vue
+++ src/views/Libreria.vue
@@ -1,11 +1,25 @@
 <template>
-    <div class="libreria">
-        libreria
+    <div class="libreria container">
+        <BooksList :books="mybooks" />
     </div>
 </template>
 
 <script>
-export default {};
+import BooksList from "./../components/BooksList";
+export default {
+    components: { BooksList },
+    data() {
+        return {
+            mybooks: [],
+        };
+    },
+    mounted() {
+        fetch("http://localhost:3000/myLibrary")
+            .then((res) => res.json())
+            .then((data) => (this.mybooks = data))
+            .catch((err) => console.log(err));
+    },
+};
 </script>
 
 <style></style>
